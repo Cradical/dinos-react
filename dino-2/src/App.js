26ee8cd4dd9b67acde8196f929b95d41063a0208
Conrad Wright <conrad@Conrads-MacBook-Pro.local>
2018-09-18 12:05:44 -0600

    header component created

---
+++ dino-2/src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import './dino.css';
+import Header from './components/header';
+
+class App extends Component {
+  constructor(props){
+    super()
+      this.state = {
+
+      }
+  }
+
+  render() {
+    return (
+      <Header />
+    );
+  }
+}
+
+export default App;
---
+++ dino-2/src/components/header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import '../dino.css'
+
+const Header = () => {
+    return(
+    <div>
+        <header>
+            <img src="../../assets/g-dino.png" alt="dino"></img>
+        </header>
+    </div>
+    )}
+
+export default Header
